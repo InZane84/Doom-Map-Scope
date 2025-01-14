4185427af743365c45ec4440945b520a235288db
InZane84 <dzcarroll@gmail.com>
2025-01-13 23:46:28 -0500

    added a header for the map-scale/delay combo boxes

--- doom_map_scope.py
+++ doom_map_scope.py
@@ -16,6 +16,7 @@ LICENSE file in the root directory of this source tree.
 
 import dearpygui.dearpygui as dpg
 import omg
+import time
 
 
 dpg.create_context()
@@ -29,6 +30,36 @@ def cb_scale_slider(sender, app_data):
 def cb_remove_drawlist():
     dpg.delete_item("drawlist")
 
+"""def cb_mapscale_slider(sender, app_data):
+    # can't get this working as needed
+    snapped_value = round(app_data / 50) * 30
+    print(f"Snapped value: {snapped_value}")
+    current_value = dpg.get_value(sender)
+    print(f"Current value: {current_value}")
+    if current_value != snapped_value:
+        dpg.set_value(sender, snapped_value)
+        wadfile.maxpixels = snapped_value
+        wadfile.plot_map(sender, snapped_value, level=None)"""
+
+def combo_callback(sender, app_data):
+    print(f"Selected Option: {app_data}")
+    match app_data:
+        case "100":
+            wadfile.maxpixels = 2000
+            wadfile.plot_map(sender, app_data, level=None)
+        case "75":
+            wadfile.maxpixels = 1500
+            wadfile.plot_map(sender, app_data, level=None)
+        case "50":
+            wadfile.maxpixels = 1000
+            wadfile.plot_map(sender, app_data, level=None)
+        case "25":
+            wadfile.maxpixels = 750
+            wadfile.plot_map(sender, app_data, level=None)
+        case "0":
+            wadfile.maxpixels = 500
+            wadfile.plot_map(sender, app_data, level=None)
+
 """def cb_open_wadfile(sender, app_data):
     
     print(f"Wadfile(s) selected: { app_data['file_path_name'] }")
@@ -39,17 +70,21 @@ class WadFile_IO:
     def __init__(self):
         self.wadfile = None
         self.level = None
+        self.maxpixels = 1000
         #self.map_names = []
         #self.map_data = []
 
     def open_wadfile(self, sender, app_data):
         self.wadfile = omg.WAD(app_data['file_path_name'])
-        dpg.set_item_label("map_viewer_id", f"Map Viewer - Wadfile: { app_data['file_path_name'] }") 
+        dpg.set_item_label("map_viewer_id", f"Map Viewer - Wadfile: { app_data['file_path_name'] }")
+        dpg.configure_item("show_map_btn", enabled=True)
         #self.map_names = self.wadfile.maps.keys()
         #self.map_data = [self.wadfile.maps[map_name] for map_name in self.map_names]
 
     def plot_map(self, sender, app_data, level):
         if self.wadfile:
+
+            dpg.delete_item("drawlist")
 
             if not self.level:
                 self.level = "MAP25"
@@ -68,7 +103,7 @@ class WadFile_IO:
             txtcol = (255, 216,   0) # yellow text
             whtcol = (255, 255, 255) # white border
 
-            maxpixels = 1000
+            #self.maxpixels = 1000
             reqscale = 0
 
             dmspawns = False
@@ -83,7 +118,7 @@ class WadFile_IO:
             ymax = max([-v.y for v in wad_level.vertexes])
             xsize = xmax - xmin
             ysize = ymax - ymin
-            scale = (maxpixels - border*2) / float(max(xsize, ysize))
+            scale = (self.maxpixels - border*2) / float(max(xsize, ysize))
 
             # tally for average scale or compare against requested scale
             if reqscale == 0:
@@ -142,7 +177,13 @@ class WadFile_IO:
                     """
 
                     dpg.draw_line((p1x, p1y), (p2x, p2y), color=color, thickness=1)
+                    #dpg.render_dearpygui_frame()
+                    #print("Drawing line: {}", {line.front})
 
+                    # DON'T DELETE THIS!===================
+                    delay = dpg.get_value("delay_slider")
+                    time.sleep(float(delay))
+                    # =====================================
                     # scale up to anti-alias
                     """
                     if alias > 1:
@@ -176,13 +217,15 @@ class WadFile_IO:
 
 wadfile = WadFile_IO()
 
+
 with dpg.file_dialog(directory_selector=False, show=False, callback=wadfile.open_wadfile, id="file_dialog_id", width=800, height=400):
-    dpg.add_file_extension(".wad")
+    dpg.add_file_extension(".wad", color=(255, 0, 0))
     dpg.add_file_extension(".iwad")
     dpg.add_file_extension(".pwad")
-    dpg.add_file_extension(".pk3")
-    dpg.add_file_extension(".wad2")
-    dpg.add_file_extension(".wad3")
+    dpg.add_file_extension("*.*")
+    #dpg.add_file_extension(".pk3")
+    #dpg.add_file_extension(".wad2")
+    #dpg.add_file_extension(".wad3")
     #dpg.add_file_extension(".deh")
     #dpg.add_file_extension(".bex")
 
@@ -194,8 +237,18 @@ with dpg.viewport_menu_bar():
 
 with dpg.window(label="Map Viewer", width=1280, height=720, id="map_viewer_id"):
     #dpg.show_documentation()
-    dpg.add_button(label="Show MAP01", callback=wadfile.plot_map)
-    dpg.add_button(label="Clear...", callback= cb_remove_drawlist)
+    with dpg.child_window(height=65, no_scrollbar=True, autosize_x=True):
+        with dpg.collapsing_header(label="Map Viewer Options"):
+            with dpg.group(horizontal=True):
+                dpg.add_button(label="Show MAP01", callback=wadfile.plot_map, tag="show_map_btn")
+                dpg.add_button(label="Clear...", callback=cb_remove_drawlist)
+                #dpg.add_slider_float(label="delay", default_value=0.000, min_value=0.000, max_value=0.100, tag="delay_slider", width=200)
+                #dpg.add_slider_int(label="Map Scale", default_value=1000, min_value=250, max_value=2500, callback=cb_mapscale_slider, clamped=True)
+            #with dpg.group(horizontal=True):
+                dpg.add_text("Delay:")
+                dpg.add_combo(width=100, items=["0.000", "0.001", "0.005", "0.010", "0.050", "0.100"], default_value="0.000", tag="delay_slider")
+                dpg.add_text("Map Scale:")
+                dpg.add_combo(width=100, items=["100", "75", "50", "25", "0"], default_value="Scale 50", callback=combo_callback)
     with dpg.menu_bar():
         with dpg.menu(label="File"):
             dpg.add_menu_item(label="Open a WAD file...", callback= lambda: dpg.show_item("file_dialog_id"))
